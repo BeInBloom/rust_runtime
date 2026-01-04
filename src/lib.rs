516a23156c5bc489138047f5580d29bb0fa1c33b
Sergei Skoropupov <donckou@gmail.com>
2026-01-04 19:56:52 +0300

    sleep funtion refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod executor;
 mod task;
 pub mod timer;
+
 pub use executor::Runtime;
 pub use timer::sleep;
--- src/timer.rs
+++ src/timer.rs
@@ -1,52 +1,160 @@
 use std::{
+    collections::BTreeMap,
+    mem,
     pin::Pin,
-    sync::{Arc, Mutex},
+    sync::{Arc, Condvar, Mutex, MutexGuard, OnceLock},
     task::{Context, Poll, Waker},
-    thread,
-    time::Duration,
+    thread::{self, spawn},
+    time::{Duration, Instant},
 };
 
-pub struct TimerFuture {
-    shared_state: Arc<Mutex<SharedState>>,
+#[derive(Default)]
+struct TimerRegistry {
+    timers: BTreeMap<Instant, Vec<Waker>>,
 }
 
-struct SharedState {
-    completed: bool,
-    waker: Option<Waker>,
+impl TimerRegistry {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn register(&mut self, deadline: Instant, waker: Waker) {
+        self.timers.entry(deadline).or_default().push(waker);
+    }
+
+    fn next_deadline(&self) -> Option<Instant> {
+        self.timers.keys().next().copied()
+    }
+
+    fn pop_ready_wakers(&mut self, now: Instant) -> Vec<Waker> {
+        let pending = self.timers.split_off(&(now + Duration::from_nanos(1)));
+        let ready_timers = mem::replace(&mut self.timers, pending);
+        ready_timers.into_values().flatten().collect()
+    }
 }
 
-impl Future for TimerFuture {
+pub struct Reactor {
+    registry: Mutex<TimerRegistry>,
+    condvar: Condvar,
+}
+
+impl Reactor {
+    fn new() -> Arc<Self> {
+        Arc::new(Reactor {
+            registry: Mutex::new(TimerRegistry::new()),
+            condvar: Condvar::new(),
+        })
+    }
+
+    fn run(self: Arc<Self>) {
+        let mut registry = self.registry.lock().unwrap();
+
+        loop {
+            let now = Instant::now();
+
+            if let Some(deadline) = registry.next_deadline() {
+                if now >= deadline {
+                    registry = self.process_ready_timers(registry, now);
+                } else {
+                    registry = self.park_thread_until(registry, deadline);
+                }
+            } else {
+                registry = self.park_thread_indefinitely(registry);
+            }
+        }
+    }
+
+    fn process_ready_timers(
+        &self,
+        mut registry: MutexGuard<TimerRegistry>,
+        now: Instant,
+    ) -> MutexGuard<'_, TimerRegistry> {
+        let wakers = registry.pop_ready_wakers(now);
+
+        drop(registry);
+
+        for waker in wakers {
+            waker.wake();
+        }
+
+        self.registry.lock().unwrap()
+    }
+
+    #[inline]
+    fn park_thread_until<'a>(
+        &self,
+        registry: MutexGuard<'a, TimerRegistry>,
+        deadline: Instant,
+    ) -> MutexGuard<'a, TimerRegistry> {
+        let now = Instant::now();
+
+        if deadline > now {
+            let duration = deadline - now;
+            self.condvar.wait_timeout(registry, duration).unwrap().0
+        } else {
+            registry
+        }
+    }
+
+    #[inline]
+    fn park_thread_indefinitely<'a>(
+        &self,
+        registry: MutexGuard<'a, TimerRegistry>,
+    ) -> MutexGuard<'a, TimerRegistry> {
+        self.condvar.wait(registry).unwrap()
+    }
+
+    #[inline]
+    fn registry(&self, deadline: Instant, waker: Waker) {
+        let mut registry = self.registry.lock().unwrap();
+        registry.register(deadline, waker);
+        self.condvar.notify_one();
+    }
+}
+
+static REACTOR: OnceLock<Arc<Reactor>> = OnceLock::new();
+
+fn get_reactor() -> &'static Arc<Reactor> {
+    REACTOR.get_or_init(|| {
+        let reactor = Reactor::new();
+        let reactor_clone = reactor.clone();
+
+        thread::Builder::new()
+            .name("timer-reactor".to_string())
+            .spawn(move || reactor_clone.run())
+            .expect("cant run reactor of thread");
+
+        reactor
+    })
+}
+
+pub struct SleepFuture {
+    deadline: Instant,
+    registered: bool,
+}
+
+impl Future for SleepFuture {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut share_state = self.shared_state.lock().unwrap();
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let now = Instant::now();
 
-        if share_state.completed {
+        if now > self.deadline {
             return Poll::Ready(());
         }
 
-        share_state.waker = Some(cx.waker().clone());
+        if !self.registered {
+            get_reactor().registry(self.deadline, cx.waker().clone());
+            self.registered = true;
+        }
+
         Poll::Pending
     }
 }
 
-pub fn sleep(duration: Duration) -> TimerFuture {
-    let shared_state = Arc::new(Mutex::new(SharedState {
-        completed: false,
-        waker: None,
-    }));
-
-    let thread_shared_state = shared_state.clone();
-    thread::spawn(move || {
-        thread::sleep(duration);
-
-        let mut shared_state = thread_shared_state.lock().unwrap();
-        shared_state.completed = true;
-
-        if let Some(waker) = shared_state.waker.take() {
-            waker.wake();
-        }
-    });
-
-    TimerFuture { shared_state }
+pub fn sleep(duration: Duration) -> SleepFuture {
+    SleepFuture {
+        deadline: Instant::now() + duration,
+        registered: false,
+    }
 }
